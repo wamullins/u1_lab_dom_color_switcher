f14e0b7b7c2b28a9c2a343a2f763fde50d908c04
Andrew <mullinsa428@gmail.com>
2023-05-12 15:56:46 -0400

    finished the dom color switcher lab

--- js/main.js
+++ js/main.js
@@ -1,1 +1,24 @@
 // Write your DOM code here!
+
+const btnR = document.querySelector(`.red`)
+const btnW = document.querySelector(`.white`)
+const btnB = document.querySelector(`.blue`)
+const btnY = document.querySelector(`.yellow`)
+let body = document.querySelector('body')
+
+
+btnR.addEventListener('click', ()=> {
+    body.style.backgroundColor = 'red'
+})
+
+btnW.addEventListener('click', ()=> {
+    body.style.backgroundColor = 'white'
+})
+
+btnB.addEventListener('click', ()=> {
+    body.style.backgroundColor = 'blue'
+})
+
+btnY.addEventListener('click', ()=> {
+    body.style.backgroundColor = 'yellow'
+})
